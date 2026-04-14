36609c5e5b41613dac5e91ac975563e06cc9f74a
Mårten Cassel <marten.cassel@gmail.com>
2026-04-14 18:39:15 +0200

    more

---
+++ scripts/get-image-and-scope.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+curl -s https://registry-1.docker.io/v2/library/alpine/manifests/sha256:59855d3dceb3ae53991193bd03301e082b2a7faa56a514b03527ae0ec2ce3a95
+
---
+++ scripts/get-status.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+https_proxy=http://172.21.22.151:9090 HTTP_PROXY=http://172.21.22.151:9090 curl -s https://registry-1.docker.io/v2/
